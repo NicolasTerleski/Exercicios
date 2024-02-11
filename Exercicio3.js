59279c2b92787bff8a55e3f1c808604e14fefe0f
Nícolas Terleski de Oliveira <159032475+NicolasTerleski@users.noreply.github.com>
2024-02-11 20:28:16 -0300

    Create Exercicio3.js

---
+++ Exercicio3.js
@@ -0,0 +1,6 @@
+let actualNum = prompt("Digite o número abaixo que você quer saber a tabuada");
+let lastNum = 10;
+
+for(let numAtual = 1; (numAtual<=lastNum); numAtual++){
+  console.log(numAtual*actualNum);
+};
